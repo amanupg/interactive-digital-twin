15600578a5392bdc80bac98dc629249be5b08228
Aman Upganlawar <105249745+amanupg@users.noreply.github.com>
2026-01-07 00:56:50 +0530

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 1. Create Nerfstudio Environment (PyTorch 2.1 / CUDA 11.8)
+# Nerfstudio Environment
 echo "Creating Environment 1: nerfstudio..."
 conda create --name nerfstudio -y python=3.10
 source activate nerfstudio
 pip install torch==2.1.2 torchvision==0.16.2 --index-url https://download.pytorch.org/whl/cu118
 pip install nerfstudio
 
-# 2. Create Brain Environment (PyTorch 2.4 / CUDA 12.1)
+# Brain Environment (PyTorch 2.4 / CUDA 12.1)
 echo "Creating Environment 2: brain..."
 conda create --name brain -y python=3.10
 source activate brain
